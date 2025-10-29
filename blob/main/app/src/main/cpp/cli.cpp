4b6b413e04729e07c251d4cc9e9ae93628aabefd
zhizunbao84 <zhizunbao84@hotmail.com>
2025-10-29 14:24:10 +0800

    Add CLI for connecting to BLE service
    Implement a CLI that connects to a BLE service and reads output.

---
+++ blob/main/app/src/main/cpp/cli.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    struct sockaddr_un addr{};
+    addr.sun_family = AF_UNIX;
+    snprintf(addr.sun_path, sizeof(addr.sun_path), "@theengs/ble");
+    // 抽象命名空间以 \0 开头
+    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        // 服务未起，尝试拉起
+        system("am startservice --user 0 -n com.theengs.bridge/.BleService");
+        sleep(2);
+        if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+            fprintf(stderr, "BLE service unavailable\n");
+            return 1;
+        }
+    }
+    FILE *f = fdopen(fd, "r");
+    char line[2048];
+    while (fgets(line, sizeof(line), f)) {
+        fputs(line, stdout);
+        fflush(stdout);
+    }
+    return 0;
+}
