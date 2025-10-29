7d9145286ec9f1f62c7fdb90eccf6d613a483306
zhizunbao84 <zhizunbao84@hotmail.com>
2025-10-29 14:34:36 +0800

    Implement BLE decoding in native.cpp

---
+++ app/src/main/cpp/native.cpp
@@ -0,0 +1,21 @@
+#include <jni.h>
+#include <string>
+#include "theengs/decoder.h"
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_theengs_bridge_BleService_decode(
+        JNIEnv *env, jobject,
+        jbyteArray adv, jint rssi, jstring jmac) {
+    jsize len = env->GetArrayLength(adv);
+    uint8_t *p = (uint8_t *) env->GetByteArrayElements(adv, nullptr);
+    const char *mac = env->GetStringUTFChars(jmac, nullptr);
+
+    JsonObject json;
+    bool ok = decodeBLE(std::string((char *)p, len), mac, rssi, json);
+
+    env->ReleaseByteArrayElements(adv, (jbyte *)p, JNI_ABORT);
+    env->ReleaseStringUTFChars(jmac, mac);
+    if (!ok) return nullptr;
+    return env->NewStringUTF(json.to<std::string>().c_str());
+}
